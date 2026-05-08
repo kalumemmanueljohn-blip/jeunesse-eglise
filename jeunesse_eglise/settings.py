1a1fc874fd9da64200c7856b5789b5a401110ea7
kalumemmanueljohn-blip <kalumemmanueljohn@gmail.com>
2026-05-08 17:35:24 +0100

    Add CSRF trusted origins for deployment

--- jeunesse_eglise/settings.py
+++ jeunesse_eglise/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production-123456789'
 DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://jmc-Cité-bethel-bandal-mouleart.onrender.com']
 
 # ============================================
 # APPLICATIONS INSTALLÉES
